019165c429c4704f8f7d991f85099004d1e65c6f
Omar Amin <omar_75@hotmail.dk>
2019-04-10 22:47:51 +0200

    Google login
    User can now log in with google and sign out as well.

--- app/src/main/java/com/chat/omar/simplechat/LogIn.java
+++ app/src/main/java/com/chat/omar/simplechat/LogIn.java
@@ -20,6 +20,11 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -27,15 +32,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class LogIn extends AppCompatActivity {
 
     private Button logInButton;
     private CallbackManager callbackManager;
     private FirebaseAuth fireAuth;
+    private GoogleSignInClient googleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +50,7 @@ public class LogIn extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
 
         fireAuth = FirebaseAuth.getInstance();
-
+        //Facebook login setup
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = findViewById(R.id.buttonFacebookLogin);
         loginButton.setReadPermissions("email", "public_profile");
@@ -51,6 +58,8 @@ public class LogIn extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d("FacebookLogin", "facebook:onSuccess:" + loginResult);
+                findViewById(R.id.buttonFacebookLogin).setVisibility(View.GONE);
+                findViewById(R.id.buttonGoogleLogin).setVisibility(View.GONE);
                 handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
@@ -68,6 +77,83 @@ public class LogIn extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.buttonFacebookSignout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fireAuth.signOut();
+                LoginManager.getInstance().logOut();
+
+                updateUI(null);
+            }
+        });
+
+        findViewById(R.id.buttonGoogleSignout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+
+        findViewById(R.id.buttonGoogleLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+
+            }
+        });
+
+        //Google setup
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Facebook
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
+                findViewById(R.id.buttonFacebookLogin).setVisibility(View.GONE);
+                findViewById(R.id.buttonGoogleLogin).setVisibility(View.GONE);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                System.out.println("CATCH");
+                updateUI(null);
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        fireAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = fireAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            updateUI(null);
+                            System.out.println("WENT WRONG");
+                        }
+                    }
+                });
     }
 
     @Override
@@ -75,6 +161,20 @@ public class LogIn extends AppCompatActivity {
         super.onStart();
         FirebaseUser user = fireAuth.getCurrentUser();
         updateUI(user);
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        fireAuth.signOut();
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
@@ -98,22 +198,25 @@ public class LogIn extends AppCompatActivity {
     }
 
     private void updateUI(FirebaseUser user) {
-
         if (user != null) {
-
+            for (UserInfo userInfo : user.getProviderData()) {
+                if (userInfo.getProviderId().equals("facebook.com")) {
+                    findViewById(R.id.buttonFacebookLogin).setVisibility(View.GONE);
+                    findViewById(R.id.buttonFacebookSignout).setVisibility(View.VISIBLE);
+                    findViewById(R.id.buttonGoogleLogin).setVisibility(View.GONE);
+                    return;
+                }
+            }
             findViewById(R.id.buttonFacebookLogin).setVisibility(View.GONE);
-            findViewById(R.id.buttonFacebookSignout).setVisibility(View.VISIBLE);
+            findViewById(R.id.buttonGoogleLogin).setVisibility(View.GONE);
+            findViewById(R.id.buttonGoogleSignout).setVisibility(View.VISIBLE);
         } else {
-
+            System.out.println("WRONG");
             findViewById(R.id.buttonFacebookLogin).setVisibility(View.VISIBLE);
             findViewById(R.id.buttonFacebookSignout).setVisibility(View.GONE);
+            findViewById(R.id.buttonGoogleLogin).setVisibility(View.VISIBLE);
+            findViewById(R.id.buttonGoogleSignout).setVisibility(View.GONE);
         }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
